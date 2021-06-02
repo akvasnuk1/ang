05ad1576c97ee03b5c1894fc931127aa8919d2db
akvasnuk1 <kvasnukandriy11@gmail.com>
2021-06-02 11:33:25 +0300

    ang2

--- src/app/service/post-resolve.service.ts
+++ src/app/service/post-resolve.service.ts
@@ -9,13 +9,13 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class PostResolveService implements Resolve<Observable<Post>> {
-  private url = 'https://jsonplaceholder.typicode.com/posts';
 
+  private url = 'https://jsonplaceholder.typicode.com/posts';
   constructor(private httpClient: HttpClient) {
   }
 
-
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Observable<Post>> | Promise<Observable<Post>> | Observable<Post> {
+
     return this.httpClient.get<Post>(this.url + '/' + route.params.id);
   }
 
