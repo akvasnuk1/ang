e3bf48024a7c8c5e1ef9c4359032869e702bd015
akvasnuk1 <kvasnukandriy11@gmail.com>
2021-06-14 13:58:29 +0300

    ang6

--- src/app/service/custom-interceptor.service.ts
+++ src/app/service/custom-interceptor.service.ts
@@ -13,7 +13,6 @@ export class CustomInterceptorService implements HttpInterceptor {
     if (req.url.includes("https://api.themoviedb.org/3/movie/top_rated&page=")) {
       let url = req.url.split("")
       let num = url.splice(50).join("")
-      console.log(num);
       pagination = req.clone({
         url: "https://api.themoviedb.org/3/movie/top_rated",
         params: req.params.set('api_key', 'dbeb8a7c94c70b10030a2bea613ec0df').set('page', num)
